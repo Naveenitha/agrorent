bad07c905ad5b99d588989a81a804b7f58cf906f
Naveenitha <ithanave628@gmail.com>
2026-03-17 00:34:35 +0530

    Update API endpoints for equipment service

--- client/src/services/equipmentService.js
+++ client/src/services/equipmentService.js
@@ -1,31 +1,31 @@
 import axiosInstance from "../api/axiosInstance";
 
 export const getAvailableEquipments = async () => {
-  const res = await axiosInstance.get("/equipment/available");
+  const res = await axiosInstance.get("/equipments/available");
   return res.data;
 };
 
 export const getMyEquipments = async () => {
-  const res = await axiosInstance.get("/equipment/myequipment");
+  const res = await axiosInstance.get("/equipments/myequipment");
   return res.data;
 };
 
 export const addEquipment = async (data) => {
-  const res = await axiosInstance.post("/equipment", data);
+  const res = await axiosInstance.post("/equipments", data);
   return res.data;
 };
 
 export const deleteEquipment = async (id) => {
-  const res = await axiosInstance.delete(`/equipment/${id}`);
+  const res = await axiosInstance.delete(`/equipments/${id}`);
   return res.data;
 };
 
 export const getEquipmentById = async (id) => {
-  const res = await axiosInstance.get(`/equipment/${id}`);
+  const res = await axiosInstance.get(`/equipments/${id}`);
   return res.data;
 };
 
 export const updateEquipment = async (id, data) => {
-  const res = await axiosInstance.put(`/equipment/${id}`, data);
+  const res = await axiosInstance.put(`/equipments/${id}`, data);
   return res.data;
 };
